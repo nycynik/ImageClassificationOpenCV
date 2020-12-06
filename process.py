eceb5e46f3a9a2821679ec59f5f8d486d03415d9
Mike Lynch <nycynik@gmail.com>
2020-12-05 23:55:34 -0800

    added 2nd image

--- process.py
+++ process.py
@@ -11,11 +11,23 @@ net = cv2.dnn.readNetFromCaffe(
     './models/bvlc_googlenet.caffemodel')
 
 # read the image
-img = cv2.imread('./images/woman-beach-sea-ocean-clam-search.jpg')
-print(img.shape)
+images = []
+images.append(cv2.imread('./images/woman-beach-sea-ocean-clam-search.jpg'))
+images.append(cv2.imread('./images/woman-hiking.jpg'))
 
 # make the blob
-blob = cv2.dnn.blobFromImage(img, 1, (224, 224))
+#blob = cv2.dnn.blobFromImages(images, 1, (224, 224))
+blob = cv2.dnn.blobFromImage(images[1], 1, (224, 224))
 
 # here we go!
 net.setInput(blob)
+
+# forward only - get inference
+output = net.forward()
+
+# show top 5
+index = np.argsort(output[0])[::-1][:5]
+
+print(f'Rank\tID\tLikely\tDescription')
+for i, id in enumerate(index):
+    print(f'{i+1}\t{id}\t{(output[0][id] * 100):.3}%\t{classes[id]}')
